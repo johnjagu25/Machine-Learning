66418d279f93826741235c40f92743dfb198ae31
unknown <johnjagu25@gmail.com>
2019-08-29 08:37:33 +0530

    multinomial : added sklearn functionality

--- ML-algo-from-scratch/naive_bayes_multinomial.py
+++ ML-algo-from-scratch/naive_bayes_multinomial.py
@@ -1,8 +1,22 @@
-import numpy as np
 from sklearn.datasets import fetch_20newsgroups 
-import re
 from collections import defaultdict
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+import numpy as np
+
+
+def preprocessing_string(documents):
+        processed_data = []
+        for index,data in enumerate(documents):
+            # keeping only alphabets
+            processed_str=re.sub('[^a-z\s]+',' ',data.lower(),flags=re.IGNORECASE)
+            # multiple spaces are replace by single space
+            processed_str=re.sub('(\s+)',' ',processed_str)
+            processed_data.append(processed_str)        
+        return np.array(processed_data)
 
 
 class NaiveBayes():
@@ -24,52 +38,43 @@ class NaiveBayes():
         self.vocab_size = None
 
     def fit(self,X,y):
-        self.labels = np.unique(y)
-        self.n_classes = self.labels.shape[0]
-        self.X = self._preprocessing_string(np.array(X))
+        self.classes = np.unique(y)
+        self.n_classes = self.classes.shape[0]
+        self.X = X
         self.y = np.array(y)        
         self.class_word_count = np.empty(self.n_classes)
         vocab_words = []
-        for index,label in enumerate(self.labels):
+        for index,cls_label in enumerate(self.classes):
             self.bow_dict.append(defaultdict(lambda :0))
-            self._addToBow(index,label)
+            self._addToBow(index,cls_label)
             self.class_word_count[index] = np.sum(list(self.bow_dict[index].values()))
             vocab_words += self.bow_dict[index].keys()
         vocab_unique = np.unique(vocab_words) 
         self.vocab_size = vocab_unique.shape[0]
 
     def predict(self,X):
-        X = self._preprocessing_string(X)
         predictions = []
         for test in X:                           
             post_prob=self._predict_row(test) 
-            predictions.append(self.labels[np.argmax(post_prob)])
+            predictions.append(self.classes[np.argmax(post_prob)])
         return np.array(predictions) 
 
     def _predict_row(self,x):
         posterior_prob = np.empty(self.n_classes)
-        for index,label in enumerate(self.labels):
+        for index,cls_label in enumerate(self.classes):
             likelihood_prob = self._cal_likelihood(x,index)
-            prior_prob = self._cal_prior(label)
+            prior_prob = self._cal_prior(cls_label)
             posterior_prob[index]=likelihood_prob + np.log(prior_prob)                                 
         return posterior_prob
 
-    def _addToBow(self,index,label):
-        filter_label = self.X[self.y == label] 
-        for list_words in filter_label :
+    def _addToBow(self,index,cls_label):
+        filter_cls_label = self.X[self.y == cls_label] 
+        for list_words in filter_cls_label :
             for word in list_words.split():
                 self.bow_dict[index][word] += 1
 
-    def _cal_prior(self,label):
-        return (self.y == label).mean()
-    
-    def _preprocessing_string(self,datas):
-        processed_data = []
-        for index,data in enumerate(datas):
-            cleaned_str=re.sub('[^a-z\s]+',' ',data.lower(),flags=re.IGNORECASE)
-            cleaned_str=re.sub('(\s+)',' ',cleaned_str)
-            processed_data.append(cleaned_str)        
-        return np.array(processed_data)
+    def _cal_prior(self,cls_label):
+        return (self.y == cls_label).mean()
 
     def _cal_likelihood(self,x,index):
         # laplace correction adding 1 to avoid zero probablity
@@ -83,16 +88,42 @@ class NaiveBayes():
 
 
 def main():
-    categories=['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med'] 
-    newsgroups_train=fetch_20newsgroups(subset='train',categories=categories)
-    X_train , y_train = newsgroups_train.data,newsgroups_train.target
-    reg = NaiveBayes()
-    reg.fit(X_train,y_train)
 
-    newsgroups_test=fetch_20newsgroups(subset='test',categories=categories)
-    X_test=newsgroups_test.data 
-    y_test=newsgroups_test.target
-    print(accuracy_score(y_test,reg.predict(X_test)))
+    categories=['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med'] 
+    newsgroups_train=fetch_20newsgroups(subset='train',categories=categories,shuffle=True)
+    newsgroups_test=fetch_20newsgroups(subset='test',categories=categories,shuffle=True)
+    X_train = preprocessing_string(np.array(newsgroups_train.data))
+    y_train = newsgroups_train.target
+    X_test  = preprocessing_string(np.array(newsgroups_test.data))
+    y_test  = newsgroups_test.target
+
+    #Python scratch implementation
+    Nb = NaiveBayes()    
+    Nb.fit(X_train,y_train)  
+    print(accuracy_score(y_test,Nb.predict(X_test)))
+
+    # Sklearn implementation
+    count_vect = CountVectorizer() 
+    X_train_count_vect = count_vect.fit_transform(X_train) 
+    y_train = newsgroups_train.target
+
+    Nb = MultinomialNB()
+    Nb.fit(X_train_count_vect,y_train)
+    X_test_counts = count_vect.transform(X_test) 
+    y_test = newsgroups_test.target
+    print(accuracy_score(y_test,Nb.predict(X_test_counts)))
+
+    # piepline implementation
+    # CountVectorizer does the preprocessing by converting text into lowercase and removing 
+    # punctuation. So input to it must be a text document
+    Nb=Pipeline([('vect', CountVectorizer()),('clf', MultinomialNB())])
+    Nb.fit(X_train,y_train)
+    print(accuracy_score(y_test,Nb.predict(X_test)))
+      
+
+
+
+
 
 
 if __name__ == "__main__":
