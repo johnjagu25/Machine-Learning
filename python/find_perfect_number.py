47a404b12fc58a762c18c1ae88b21135b435800f
unknown <johnjagu25@gmail.com>
2019-10-26 10:33:49 +0530

    perfect number

--- python/find_perfect_number.py
+++ python/find_perfect_number.py
@@ -6,10 +6,10 @@
 def perfect_number(n):
     sum_of_digit = 0
     total_val = ""
-    for val in str(n):
-        v = int(val)
-        sum_of_digit += v
-        total_val += val
+    for character in str(n):
+        digit = int(character)
+        sum_of_digit += digit
+        total_val += character
     last_digit =  abs(10 - sum_of_digit)
     return None if sum_of_digit > 10 else total_val + str(last_digit)
 
